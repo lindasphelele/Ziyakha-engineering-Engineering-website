56bf0f704b31e17b50d517b3a2876a7295c869d2
lindasphelele <106629213+lindasphelele@users.noreply.github.com>
2026-01-26 13:26:45 +0200

    Update script.js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,1 +1,13 @@
-
+// Smooth scroll for anchor links
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a.nav-link[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      const targetId = link.getAttribute('href');
+      const el = document.querySelector(targetId);
+      if (el) {
+        e.preventDefault();
+        window.scrollTo({ top: el.offsetTop - 70, behavior: 'smooth' });
+      }
+    });
+  });
+});
